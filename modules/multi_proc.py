9077f85efc67c4c3489363ba26a8de73c16a5bb7
Idir.D <idirdehmous@gmail.com>
2024-12-17 18:02:14 +0100

    move gc distances to a new class dist_matrix

--- modules/multi_proc.py
+++ modules/multi_proc.py
@@ -5,7 +5,7 @@ import  re
 import  numpy        as np 
 from    itertools    import *
 import  multiprocessing  as mp 
-import resource  
+import  resource  
 
 sys.path.insert(0, "/hpcperm/cvah/tuning/ww_oslo/pyodb_1.1.0/build/lib.linux-x86_64-cpython-310" )
 
@@ -25,98 +25,12 @@ env.InitEnv ()
 
 # --> NOW pyodb could be imported  !
 from pyodb   import  odbFetch
-from pyodb   import  gcDist 
+
+
 
 from build_sql   import  SqlHandler
 from obstype_gen import  ObsType 
-
-def Flatten(list_of_items):
-    "Flatten one level of nesting."
-    return chain.from_iterable(list_of_items)
-
-
-
-def haversine(lat1, lon1, lat2, lon2):
-    R = 6378.137  # Radius of Earth in kilometers
-    lat1, lon1, lat2, lon2 = map(np.radians, [lat1, lon1, lat2, lon2])  # Convert to radians
-    dlat = lat2 - lat1
-    dlon = lon2 - lon1
-    a = np.sin(dlat / 2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2)**2
-    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
-    
-    # Distance in kilometers
-    return R * c
-
-
-
-def Distances_in_chunk(chunk_data, full_data, start_idx, chunk_size):
-    """
-    Compute great circle distances between all points in a chunk with all other points.
-    Arguments:
-    - chunk_data: Data for the current chunk (latitudes and longitudes).
-    - full_data: Full dataset for computing distances across chunks.
-    - start_idx: Starting index of the chunk in the full data.
-    - chunk_size: The number of points in the chunk.
-    Returns:
-    - A distance matrix chunk for the given chunk.
-    """
-    distances_chunk = np.zeros((chunk_size, len(full_data)))
-    #an_chunk=np.zeros((chunk_size, len(full_data)))
-    #fg_chunk=np.zeros((chunk_size, len(full_data)))
-
-    for i in range(chunk_size):
-        lat1, lon1 = chunk_data[i]
-        lat2, lon2 = full_data [:, 0] , full_data[:, 1]         # Full data lat/lon
-
-        # The distance is computed with haversine formula 
-        # In the R the great circle is computed with Vicenty's formula 
-        # Could be implemented later
-        # The diffrence in accuracy is just 0.01 %
-        distances_chunk[i, :] = haversine(lat1, lon1, lat2, lon2 ) 
-
-#        lat1, lon1, lat2, lon2 = map(np.radians, [lat1, lon1, lat2, lon2])
-#        point1 =(lat1, lon1)
-#        point2 =(lat2, lon2)
-#        distances_chunk[i, :] = geodesic(point1, point2).kilometers.all()
-    return start_idx, distances_chunk
-
-
-
-
-
-
-
-def ComputeDistances(data, chunk_size=1000):
-    """
-    Compute the full distance matrix for all points in the data using chunks and multiprocessing.
-    Arguments:
-    - data: Latitudes and longitudes of points (n_samples, 2).
-    - chunk_size: Number of samples to process in each chunk.
-    Returns:
-
-    - A distance matrix of shape (n_samples, n_samples).
-    """
-    n_samples = len(data)
-    distance_matrix = np.zeros((n_samples, n_samples))
-
-    # Split the data into chunks for parallel computation
-    with mp.Pool(processes=mp.cpu_count()) as pool:
-        results = []
-
-        for start_idx in range(0, n_samples, chunk_size):
-            chunk_end_idx = min(start_idx + chunk_size, n_samples)
-            chunk_data    = data[start_idx:chunk_end_idx]
-
-            # Submit task to the pool
-            results.append(pool.apply_async(
-                Distances_in_chunk,  (chunk_data, data, start_idx, chunk_end_idx - start_idx) ))
-
-        # Collect all results
-        for result in results:
-            start_idx, distances_chunk = result.get()
-            distance_matrix[start_idx:start_idx+distances_chunk.shape[0], :] = distances_chunk
-    return distance_matrix
-
+from dist_matrix import  gcDistance 
 
 
 
@@ -126,7 +40,6 @@ class MpRequest:
         self.dbpath = dbpath 
         self.query  = sql_query
         self.varobs = varobs 
-        print( self.varobs ) 
         return None 
 
     def ParallelEnv(self):
@@ -139,6 +52,7 @@ class MpRequest:
         else:
            resource.setrlimit(resource.RLIMIT_CPU, ( -1, -1) )
         return ncpu  , th_info  
+
 
 
 
@@ -163,17 +77,24 @@ class MpRequest:
            return None 
 
 
+                                                  
+
+    def Flatten(self, list_):
+        "Flatten one level of nesting."
+        return chain.from_iterable(list_)
+
+
+
     def Chunk( self, lst , chunk_size   ):
         if len(lst)    > chunk_size  and chunk_size >=2 :   # nchunks should be 2 at least
            return [lst[i:i+chunk_size] for i in range(0, len(lst) , int(chunk_size ) )]
         else:
-           #len(lst)   <= int(chunk_size):
            return lst
 
 
   
     def Seqno (self  ):
-        # Get the seqno numbers of the rows & entries  
+        # Get the rows sequence number in ODB 
         sql_seqno=  self.AlterQuery(self.query )  
         query_file=None 
         nfunc     =0 
@@ -193,11 +114,12 @@ class MpRequest:
            sys.exit (1)
 
 
-    def SelectBySeqno   (self ):
+    def SelectBySeqno   (self ,   sq1, sq2 ):
         os.environ["ODB_MAXHANDLE"]= "200"
-#        print( "Process id {}  ".format(  mp.current_process())  , "seqno rows range :",  sq1, sq2  )
-#        print( "Extract ODB data from {} to {}  ".format( sq1, sq2 )     )
-        nfunc    =0
+        #print( "Process id {}  ".format(  mp.current_process())  , "seqno rows range :",  sq1, sq2  )
+        pp=os.getpid() 
+        print("Get ODB rows by seqno. {} to {}  process: {}".format( sq1, sq2 , pp  ) )
+        nfunc    = 2
         query_file=None 
         pool     = None   
         
@@ -217,14 +139,10 @@ class MpRequest:
                    verbose , 
                     header  
                         )
-
-        # Handle rows 
-        out_rows = [   row[:-2] for row  in rows ]
-        return  out_rows 
+        return  rows 
 
 
-    def DispatchQuery(self , nchunk , nproc ):
-       
+    def DispatchQuery(self , nchunk , nproc ):       
        # N proc pools 
        # Set default parameters 
        ncpu     =self.ParallelEnv()[0]
@@ -253,42 +171,61 @@ class MpRequest:
           print("Failed to split the seqno list into chunks " )
           sys.exit (0)
 
-       sq_sets=[[1557, 1560], [1561, 1564] ]
+       # Run in prallel pools (of processes not ODB pools !!!)
+       if nchunk >1    and  nproc  > 1 :   # Parallel
+          with mp.Pool(processes=pool_size) as _pool:
+               out = _pool.starmap(self.SelectBySeqno  ,[ (seq[0], seq[1]) for  seq in sq_sets]    )
+       rows=list( self.Flatten(out ) )  
+       self.ClosePool(_pool)
+       return rows 
 
-       pool_size =1 
-       rows  =  self.SelectBySeqno ()
-       lats= [ row[4]   for row in rows   ]
-       lons= [ row[5]   for row in rows   ]
-       an_d= [ row[9 ]  for row in rows   ]
-       fg_d= [ row[10]  for row in rows   ]
+    def ClosePool (self, pool ):
 
-
-#       llat=[ p for p in  product(  [ row[4 ]    for row in rows] ,  repeat =2 )] 
-#       llon=[ p for p in  product(  [ row[5 ]    for row in rows] ,  repeat =2 )]
-
-       # Build a numpy array 
-       idx=[]
-       [  idx.append(i)   for i in  product(range(len(rows)) , repeat=2) ]
-       d1=[ i[0] for i in idx ]
-       d2=[ i[1] for i in idx ]
-
-       #an_d=[ p for p in  product(  [ row[9 ]    for row in rows] ,  repeat =2 )]  
-       #fg_d=[ p for p in  product(  [ row[10 ]   for row in rows] ,  repeat =2 )]
-       an1=[i[0] for i in product(an_d , repeat=2)    ]
-       an2=[i[1] for i in product(an_d , repeat=2)    ]
-
-       fg1=[i[0] for i in product(fg_d , repeat=2)    ]
-       fg2=[i[1] for i in product(fg_d , repeat=2)    ]
+        pool.close()
+        pool.join()
+        del pool 
+        return None 
 
 
-       latlon=np.array( [lats,lons] ).T
-       
-       dist=ComputeDistances ( latlon  , chunk_size=2  )
 
-       gcdist=list(dist.reshape(len(lats)**2 ) )
-       data=[d1,d2, gcdist,  an2 , an1 , fg2, fg1  ]
-       data_arr = np.array( data  ).T
-       return  data_arr 
+
+
+
+
+class GatherRows():
+      def __init__(self):
+          self.gc = gcDistance ()
+          return None 
+
+
+      def Rows2Array(self, rows ):
+          lats= [ row[4]   for row in rows   ]
+          lons= [ row[5]   for row in rows   ]
+          an_d= [ row[9 ]  for row in rows   ]
+          fg_d= [ row[10]  for row in rows   ]
+
+          # Build a numpy array 
+          idx=[]
+          [  idx.append(i)   for i in  product(range(len(rows)) , repeat=2) ]
+          d1=[ i[0] for i in idx ]
+          d2=[ i[1] for i in idx ]
+
+          an1=[i[0] for i in product(an_d , repeat=2)    ]
+          an2=[i[1] for i in product(an_d , repeat=2)    ]
+
+          fg1=[i[0] for i in product(fg_d , repeat=2)    ]
+          fg2=[i[1] for i in product(fg_d , repeat=2)    ]
+
+          # Matrix distances 
+          latlon=np.array( [lats,lons] ).T
+          dist=self.gc.ComputeDistances ( latlon  , chunk_size=10  )
+
+          gcdist=list(dist.reshape(len(lats)**2 ) )
+
+          data=[d1,d2, gcdist,  an2 , an1 , fg2, fg1  ]
+          data_arr = np.array( data  ).T
+          return  data_arr 
+
 
 
 
