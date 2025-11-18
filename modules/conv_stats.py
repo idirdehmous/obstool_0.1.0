b0140aa2a43a5f79198671c9f88fe49cf21d9961
Idir Dehmous <idir.dehmous@meteo.be>
2025-11-18 14:36:49 +0100

    Fix the list bug in DHLStat class

--- modules/conv_stats.py
+++ modules/conv_stats.py
@@ -1,29 +1,31 @@
 #-*-coding:utf-8 -*- 
 import os 
-os.environ["NUMEXPR_MAX_THREADS"]="256"
-
 import pandas as pd 
 import numpy as np 
-from collections import defaultdict 
+from   collections import defaultdict 
 
+# obstool modules 
 from handle_df  import GroupDf  
 
 
 
 
+__all__=["DHLStat" ]
+
+
+
 class DHLStat:
     """
-    Class :  Compute the diffrents statistics
-             covariance, correlation and standard deviations 
-             from FG1, FG2 , OA1 , OA2 in observation space 
-             After the method of 
-             Desroziers , Hollingsworth,Lonneberg ( DHL )
+    Class :  @Compute the diffrent statistics
+              covariance, correlation and standard deviations 
+              from FG1, FG2,OA1 and OA2 departures in observation space 
+
+             @Following the methods of :
+              Desroziers , Hollingsworth,Lonneberg ( DHL )
     """
+    def __init__(self, df  , max_dist =100, bin_dist =10 , delta_t =60 ):
 
-    def __init__(self, df   ,new_max_dist =100, new_bin_dist =10 , delta_t =60 ):
-
-    
-           # STATS
+        # STATS
         # GET THE STATS IN THE MIDDLE OF THE BIN SQUARE  (the maximum distance and bin interval could be changed !!)
         # DEFAULT VALUES IN class 
         self.dist_max=100 #Km
@@ -31,31 +33,30 @@ class DHLStat:
 
         # OVERWRITE IF DIFFERENT 
 
-        if new_max_dist != 100:
-           self.dist_max =  new_max_dist
-           print("New value for maximum distance has been set. Maximum value for distance =" , self.dist_max )
+        if max_dist != 100:
+           self.dist_max =  max_dist
+           print("New value for maximum distance has been set. Maximum distance value = {} Km".format( self.dist_max ) )
         else:
-           print("Default value for maximum distance is used :" , self.dist_max )
+           print("Default value for maximum distance is used : {} Km".format(  self.dist_max )  )
 
-        if new_bin_dist != 10 : 
-           self.bin_int =  new_bin_dist
-           print("New value for maximum distance has been set. bin_interval=" , self.bin_int)
+        if bin_dist != 10 : 
+           self.bin_int =  bin_dist
+           print("New binning distance has been set.  bin_interval= {} Km".format(  self.bin_int) )
         else:
-           print("Default value for binning interval is used :" , self.bin_int )
+           print("Default value for binning interval is used : {} Km".format(  self.bin_int ) )
 
-        self.gp       =GroupDf ()
+        self.gp =GroupDf ()
         self.merged_df=df 
         return None 
 
 
     def getCov(self , var , inplace=None  ):
-        #d1,d2, d3, d4, d5, d6, d7, d8 , dobs , dist_list , dte_col =self.gp.GroupByBins (self.merged_df , self.dist_max  ,self.bin_int )
-        d1,d2, d3, d4, d5, d6, d7, d8 , dobs , dist_list =self.gp.GroupByBins (self.merged_df , self.dist_max  ,self.bin_int )
-        
+        d1,d2, d3, d4, d5, d6, d7, d8 , dobs , dist_list, dte, var =self.gp.GroupByBins (self.merged_df , self.dist_max  ,self.bin_int )
+
         # Nobs 
         nobs=np.asarray(dobs )
+
         # Varname 
-        #var_col= pd.Series ( [str(var )] * 10  )
         # HL (Holingsworth-LÖnnberg )
         t1 =  np.divide  (d5 ,dobs) 
         t2 =  np.divide  ( np.multiply(d2 ,d3  ) , np.power( dobs, 2 ))
@@ -71,28 +72,30 @@ class DHLStat:
         tr1 = np.divide  ( d4 , dobs  )
         tr2 = np.divide  (np.multiply( d1 , d3 ) , np.power(dobs , 2 ) )
         cov_drR = np.subtract(tr1 , tr2 )
-        
 
-
+        # Covariances 
         df_cov =  pd.DataFrame ({ 
-          #                        "date"    : dte_col  , 
+                                  "var"     : list(var), 
+                                  "period"  : list(dte), 
                                   "nobs"    : nobs     , 
+                                  "dist"    : list(dist_list),  
                                   "COV_HL"  : cov_hl   ,
                                   "COV_DR-B": cov_drB  ,
                                   "COV_DR-R": cov_drR  } )
 
         if inplace ==True :
-           return cov_hl , cov_drB, cov_drR  # Inner class 
+           return cov_hl , cov_drB, cov_drR  # inside the class 
         else: 
            return df_cov  
         
 
-    def getSigma ( self , var , inplace =None ):
+    def getSig  ( self , var , inplace =None ):
 
-#        d1,d2, d3, d4, d5, d6, d7, d8 , dobs , dist_list , dte_col =self.gp.DataByBins (self.merged_df , var , self.dist_max  ,self.bin_int )
-        d1,d2, d3, d4, d5, d6, d7, d8 , dobs , dist_list =self.gp.DataByBins (self.merged_df , self.dist_max  ,self.bin_int )
+        d1,d2, d3, d4, d5, d6, d7, d8 , dobs , dist_list, dte, var  =self.gp.GroupByBins (self.merged_df , self.dist_max  ,self.bin_int )
+
         # Nobs 
         nobs = np.asarray( dobs )
+
         # SIGMA FG1
         st1=np.divide( d6 , dobs)
         st2=np.divide( d2 , dobs)**2
@@ -110,26 +113,26 @@ class DHLStat:
         st2=np.divide( d1 ,dobs )**2
         sigma_a1=np.sqrt( np.subtract(st1, st2  )) 
         df_sig =pd.DataFrame({ 
-         #                      "date"     : dte_col    ,
+                               "var"      : list(var)  ,                
+                               "period"   : list(dte)  ,
                                "nobs"     : nobs       ,
-                               "dist"     : dist_list  ,
+                               "dist"     : list(dist_list)  ,
                                "sigma_fg1": sigma_fg1  ,
                                "sigma_fg2": sigma_fg2  ,
                                "sigma_a1" : sigma_a1
                                 })
         if inplace ==True:
-           return sigma_fg1 , sigma_fg2, sigma_a1    # Inner class 
+           return sigma_fg1 , sigma_fg2, sigma_a1    # used inside the class 
         else:
            return df_sig
 
 
     def getCor (self,var ,  inplace=None ):
-        #_,_, _, _, _, _, _, _  , dobs , dist_list , dte_col =self.gp.DataByBins (self.merged_df ,var ,  self.dist_max  ,self.bin_int )
-
-        _,_, _, _, _, _, _, _ , dobs , dist_list =self.gp.DataByBins (self.merged_df , self.dist_max  ,self.bin_int )
+        _,_, _, _, _, _, _, _ , dobs , dist_list, dte, var  =self.gp.GroupByBins (self.merged_df , self.dist_max  ,self.bin_int )
         cov_hl, cov_drB, cov_drR =self.getCov  ( var , inplace =True )
-        sfg1 , sfg2 , sa1        =self.getSigma( var , inplace =True )
+        sfg1 , sfg2 , sa1        =self.getSig  ( var , inplace =True )
 
+        # N obs 
         nobs=np.asarray( dobs )
 
         # CORRELATIONS
@@ -139,9 +142,10 @@ class DHLStat:
 
 
         df_cor=pd.DataFrame({ 
-        #        "date"     : dte_col    ,
+                "var"      :list( var ) , 
+                "period"   :list( dte ) ,
                 "nobs"     : nobs       ,
-                "dist"     : dist_list  ,
+                "dist"     :list(dist_list ) ,
                 "COR_HL"   : cor_hl     ,
                 "COR_DR-B" : cor_drB    ,
                 "COR_DR-R" : cor_drR
@@ -155,28 +159,30 @@ class DHLStat:
 
 
     def getStatFrame (self , var ):
-#        _,_, _, _, _, _, _, _ , dobs , dist_list, var_col , dte_col =self.GroupDF ()
-        _,_, _, _, _, _, _, _  , dobs , dist_list =self.gp.DataByBins (self.merged_df,  self.dist_max  ,self.bin_int )
+        _,_, _, _, _, _, _, _  , dobs , dist_list, dte, var  =self.gp.GroupByBins (self.merged_df,  self.dist_max  ,self.bin_int )
+
+        # N obs 
         nobs=np.asarray(dobs )
+
+        # Gather all Statistics  
         cov_hl, cov_drB, cov_drR     =self.getCov  ( var , inplace =True )
         cor_hl, cor_drB, cor_drR     =self.getCor  ( var , inplace =True )
-        sigma_fg1,sigma_fg2,sigma_a1 =self.getSigma( var , inplace =True )
+        sigma_fg1,sigma_fg2,sigma_a1 =self.getSig  ( var , inplace =True )
        
-        # DataFrame containing The Desroziers , Hollingsworth/Lonnberg stats 
-        #drhl_frame={ #"var"      :var_col      , 
-        #             "date"     :dte_col      , 
-        #             "nobs"     :nobs         ,
-        #             "dist"     :dist_list    , 
-        drhl_frame={ "COV_HL"   :cov_hl       , 
+        # DataFrame: Contains  the Desroziers , Hollingsworth/Lonnberg   Cov, Sigma and Corr 
+        drhl_frame={ 
+                     "nobs"     :nobs         ,
+                     "COV_HL"   :cov_hl       , 
                      "COV_DR-B" :cov_drB      ,
                      "COV_DR-R" :cov_drR      ,
                      "sigma_FG1":sigma_fg1    ,
                      "sigma_FG2":sigma_fg2    ,
                      "sigma_a1" :sigma_a1     ,
                      "COR_HL"   :cor_hl       , 
-                     "COR_DR-B" :cor_drB      , 
-                     "COR_DR-R" :cor_drR
+                     "COR_DR-R" :cor_drR      ,
+                     "COR_DR-B" :cor_drB      
                   }
 
+        # Return statistics DF 
         stat_frame =pd.DataFrame (   drhl_frame  )    # To Be Plotted   !!
         return stat_frame 
